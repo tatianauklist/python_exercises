df1e779d88f962fdda258a0d2aee306edef6f6e1
Tatiana <122945249+tatianauklist@users.noreply.github.com>
2025-10-23 14:54:40 -0500

    Delete Strings/Strings directory

--- Strings/Strings/email_username.py
+++
@@ -1,8 +0,0 @@
-email = input("Please input your email: ")
-username = ""
-index = 0
-
-while email[index] != '@':
-    username += email[index]
-    index += 1
-print(f"Your username is {username}")
